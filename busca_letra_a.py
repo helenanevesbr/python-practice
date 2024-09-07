d8f6bba10ef4d6131a7c2131d5ea04d12109703d
Helena Neves <helenaneves.br@gmail.com>
2024-09-07 19:46:56 -0300

    feat(busca texto): Busca letra 'a' no texto

--- busca_letra_a.py
+++ busca_letra_a.py
@@ -1,16 +1,16 @@
-def contagem_letra_a(frase:str)->int:
-    return 3
+def contagem_letra_a(texto:str)->int:
+    return sum(1 for letra in texto if letra.lower() == 'a')
 
 
-mensagem_entrada = 'Digite uma frase, ou digite "S" para Sair: \n'
+mensagem_entrada = 'Digite uma texto, ou digite "S" para Sair: \n'
 
-frase = input(mensagem_entrada)
+texto = input(mensagem_entrada)
 
-while frase.lower() != 's':
-    contagem = contagem_letra_a(frase=frase)
+while texto.lower() != 's':
+    contagem = contagem_letra_a(texto=texto)
     if contagem < 1:
         print(f'Letra "a" não aparece na frase')
-        frase = input(mensagem_entrada)
+        texto = input(mensagem_entrada)
     else:
         print(f'Letra "a" aparece {contagem} vez{'es' if contagem > 1 else ''} na frase')
-        frase = input(mensagem_entrada)
+        texto = input(mensagem_entrada)
