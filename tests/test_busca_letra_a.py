4579c1f524b2638ae28c3df66eaaed8932e45011
Helena Neves <helenaneves.br@gmail.com>
2024-09-07 20:28:57 -0300

    fix(test): contagem correta no teste unitário

--- tests/test_busca_letra_a.py
+++ tests/test_busca_letra_a.py
@@ -7,7 +7,7 @@ def test_contagem_letra_a_sem_a():
     assert contagem_letra_a("bcd efg") == 0, "Deve retornar 0 se não há 'a' no texto"
 
 def test_contagem_letra_a_com_varias_a():
-    assert contagem_letra_a("Ana tem uma árvore à beira da estrada.") == 12, "Deve contar corretamente as variações de 'a'"
+    assert contagem_letra_a("Ana tem uma árvore à beira da estrada.") == 9, "Deve contar corretamente as variações de 'a'"
 
 def test_contagem_letra_a_com_caracteres_especiais():
-    assert contagem_letra_a("@b@c@d@ e#f$g% ^&*(") == 2, "Caracteres especiais não devem afetar a contagem de 'a'"
+    assert contagem_letra_a("@b@c@d@ e#f$g% ^&*(") == 0, "Caracteres especiais não devem afetar a contagem de 'a'"
